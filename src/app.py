43bf050ff7a40c4bb3cb666ac203370c539c90bb
Jerry <jiakun.zhang@demeas.com>
2025-08-04 17:39:43 +0800

    init

---
+++ src/app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from PySide6.QtWidgets import QMainWindow
+
+
+class MainViewWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+
+    def initUI(self):
+        pass
